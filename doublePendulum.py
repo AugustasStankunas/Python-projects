b2407f7e6577a2d960ba87cb60ae4f6b56fca1b8
Augustas Stankūnas <149320290+AugustasStankunas@users.noreply.github.com>
2024-06-20 13:56:48 +0300

    Update doublePendulum.py

--- doublePendulum.py
+++ doublePendulum.py
@@ -16,7 +16,7 @@ def initialize(space):
 
 #creates the first ball at position pos and attaches it to the top body by a pin joint
 def firstBall(topBody, space, pos):
-    circleBody = pymunk.Body(100, 500, body_type=pymunk.Body.DYNAMIC)
+    circleBody = pymunk.Body(40, 50, body_type=pymunk.Body.DYNAMIC)
     circleBody.position = pos
     circleShape = pymunk.Circle(circleBody, 15)
     joint = pymunk.PinJoint(topBody, circleBody)
@@ -24,7 +24,7 @@ def firstBall(topBody, space, pos):
 
 #creates the second ball at position pos and attaches it to the first ball by a pin joint
 def secondBall(circle1, space, pos):
-    circleBody = pymunk.Body(80, 500, body_type=pymunk.Body.DYNAMIC)
+    circleBody = pymunk.Body(70, 50, body_type=pymunk.Body.DYNAMIC)
     circleBody.position = pos
     circleShape = pymunk.Circle(circleBody, 15)
     joint = pymunk.PinJoint(circle1, circleBody)
@@ -38,28 +38,28 @@ def DrawText(text, x : int, y : int, screen):
 
 #draws all the text on the screen for the sliders and keyboard controls
 def DrawAllText(screen, slider1, slider2, slider3, slider4, slider5):
-    DrawText(f'Mass of first ball: {slider1.getValue()}', 900, 20, screen)
-    DrawText(f'Mass of second ball: {slider2.getValue()}', 880, 100, screen)
-    DrawText(f'Momentum of first ball: {slider3.getValue()}', 860, 180, screen)
-    DrawText(f'Momentum of second ball: {slider4.getValue()}', 830, 260, screen)
-    DrawText(f'Gravity: {slider5.getValue()}', 930, 340, screen)
+    DrawText(f'Mass of first ball: {slider1.getValue():.2f}', 900, 20, screen)
+    DrawText(f'Mass of second ball: {slider2.getValue():.2f}', 880, 100, screen)
+    DrawText(f'Momentum of first ball: {slider3.getValue():.2f}', 860, 180, screen)
+    DrawText(f'Momentum of second ball: {slider4.getValue():.2f}', 830, 260, screen)
+    DrawText(f'Gravity: {slider5.getValue():.2f}', 930, 340, screen)
     DrawText('Press D for default values', 860, 420, screen)
 
 #updates the properties of the balls and gravity
 def UpdateProperties(firstMass, firstMomentum, secondMass, secondMomentum, gravity, space):
-    space.bodies[1].mass = firstMass
-    space.bodies[1].moment = firstMomentum
-    space.bodies[2].mass = secondMass
-    space.bodies[2].moment = secondMomentum
-    space.gravity = (0.0, gravity)
+    space.bodies[1].mass = 40 * firstMass
+    space.bodies[1].moment = 50 *firstMomentum
+    space.bodies[2].mass = 70 * secondMass
+    space.bodies[2].moment = 50 * secondMomentum
+    space.gravity = (0.0, 300 * gravity)
 
 #sets the default values for the sliders 
 def SetDefaultValues(slider1, slider2, slider3, slider4, slider5):
-    slider1.setValue(100)
-    slider2.setValue(80)
-    slider3.setValue(500)
-    slider4.setValue(500)
-    slider5.setValue(500)
+    slider1.setValue(1)
+    slider2.setValue(1)
+    slider3.setValue(1)
+    slider4.setValue(1)
+    slider5.setValue(1)
 
 def main():
     #initializing pygame screen and pymunk space
@@ -79,11 +79,11 @@ def main():
     circleCount = 0
 
     #initializing the sliders, used to control the mass, inertia of the balls and gravity
-    slider1 = Slider(screen, 880, 70, 200, 10, min=1, max=500, step=10)
-    slider2 = Slider(screen, 880, 150, 200, 10, min=1, max=500, step=10)
-    slider3 = Slider(screen, 880, 230, 200, 10, min=1, max=1000, step=50)
-    slider4 = Slider(screen, 880, 310, 200, 10, min=1, max=1000, step=50)
-    slider5 = Slider(screen, 880, 390, 200, 10, min=1, max=1000, step=50)
+    slider1 = Slider(screen, 880, 70, 200, 10, min=0, max=3, step=0.05, initial = 1)
+    slider2 = Slider(screen, 880, 150, 200, 10, min=0, max=3, step=0.05, initial = 1)
+    slider3 = Slider(screen, 880, 230, 200, 10, min=0, max=3, step=0.05, initial = 1)
+    slider4 = Slider(screen, 880, 310, 200, 10, min=0, max=3, step=0.05, initial = 1)
+    slider5 = Slider(screen, 880, 390, 200, 10, min=0, max=3, step=0.05, initial = 1)
 
     SetDefaultValues(slider1, slider2, slider3, slider4, slider5)
     while True:
@@ -117,7 +117,7 @@ def main():
         #start of every frame - clearing the screen, updating the simulation, drawing the simulation and updating the sliders
         screen.fill((255,255,255))
         if not pause:
-            space.step(1/30)  
+            space.step(1/20)  
 
         space.debug_draw(draw_options)
         pygame_widgets.update(pygame.event.get())
@@ -126,7 +126,7 @@ def main():
         if circleCount == -1:
             UpdateProperties(slider1.getValue(), slider2.getValue(), slider3.getValue(), slider4.getValue(), slider5.getValue(), space)
 
-        clock.tick(50)   
+        clock.tick(60)   
         pygame.display.flip()       
 
 if __name__ == '__main__':
